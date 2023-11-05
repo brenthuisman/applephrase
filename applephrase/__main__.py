2cbd0d6f785cb23a8b63c507ecfe0381f818e9ef
Brent Huisman <brent@huisman.pl>
2023-11-05 20:32:44 +0100

    Fix workaround for German and Cyrillic.

--- applephrase/__main__.py
+++ applephrase/__main__.py
@@ -2,18 +2,18 @@ import secrets, string, re, argparse
 from wuggy import WuggyGenerator
 
 langdict = {
-    'nl': "orthographic_dutch",
-    'fr': "orthographic_french",
-    'en': "orthographic_english",
-    'de': "orthographic_german",
-    'pl': "orthographic_polish",
-    'eu': "orthographic_basque",
-    'es': "orthographic_spanish",
-    'it': "orthographic_italian",
-    'sc': "orthographic_serbian_cyrillic",
-    'sl': "orthographic_serbian_latin",
-    'vt': "orthographic_vietnamese",
-    'ee': "orthographic_estonian",
+    "nl": "orthographic_dutch",
+    "fr": "orthographic_french",
+    "en": "orthographic_english",
+    "de": "orthographic_german",
+    "pl": "orthographic_polish",
+    "eu": "orthographic_basque",
+    "es": "orthographic_spanish",
+    "it": "orthographic_italian",
+    "sc": "orthographic_serbian_cyrillic",
+    "sl": "orthographic_serbian_latin",
+    "vt": "orthographic_vietnamese",
+    "ee": "orthographic_estonian",
 }
 
 def parsedicefile(fname,minlen,maxlen):
@@ -37,36 +37,37 @@ def wugglify(args):
 
     words = parsedicefile(f"{g.language_plugin_data_path}/{langf}.txt",args.min,args.max)
 
-    while True:
-        try:
-            pws = []
-            for j in range(args.np):
-                pw = args.sep.join([g.generate_classic([secrets.choice(words)],ncandidates_per_sequence=1)[0]["pseudoword"].capitalize() if not args.nocaps else secrets.choice(words) for i in range(args.nw)]) + args.sep + ''.join([secrets.choice(string.digits + string.ascii_letters) for i in range(args.nc)])
-                pws.append(pw)
-        except IndexError as e:
-            raise e
-        except Exception:
-            continue
-        break
-    return pws
+    passphrases = []
+    for j in range(args.np):
+        pseudowords = []
+        while len(pseudowords) < args.nw:
+            try:
+                pseudowords.append(g.generate_classic([secrets.choice(words)],ncandidates_per_sequence=1)[0]["pseudoword"])
+            except:
+                # Sometimes g.generate_classic returns empty list.
+                # Seems to only happen with German and Serbocroatian Cyrillic.
+                continue
+        pw = args.sep.join([i.capitalize() for i in pseudowords] if not args.nocaps else pseudowords) + args.sep + "".join([secrets.choice(string.digits + string.ascii_letters) for i in range(args.nc)])
+        passphrases.append(pw)
+    return passphrases
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
                         prog="applephrase",
                         description="Generate memorable passphrases using pseudowords.")
-    parser.add_argument('lang', type=str, choices=list(langdict.keys()), help="Pick a language")
-    parser.add_argument('--min', type=int, default=5, help="Minimum word length")
-    parser.add_argument('--max', type=int, default=10, help="Maximum word length")
-    parser.add_argument('--nw', type=int, default=2, help="Number of words")
-    parser.add_argument('--nc', type=int, default=6, help="Number of characters in the random string")
-    parser.add_argument('--np', type=int, default=20, help="Number of passphrases to generate")
-    parser.add_argument('--sep', type=str, default="-", help="Separator")
-    parser.add_argument('--nocaps', help="Don't capitalize words.", action='store_true')
+    parser.add_argument("lang", type=str, choices=list(langdict.keys()), help="Pick a language")
+    parser.add_argument("--min", type=int, default=5, help="Minimum word length")
+    parser.add_argument("--max", type=int, default=10, help="Maximum word length")
+    parser.add_argument("--nw", type=int, default=2, help="Number of words in a passphrase")
+    parser.add_argument("--nc", type=int, default=6, help="Number of characters in the random string")
+    parser.add_argument("--np", type=int, default=20, help="Number of passphrases to generate")
+    parser.add_argument("--sep", type=str, default="-", help="Word separator")
+    parser.add_argument("--nocaps", help="Don't capitalize words.", action="store_true")
 
     args = parser.parse_args()
 
-    pws = wugglify(args)
+    passphrases = wugglify(args)
 
-    for pw in pws:
-        print(pw)
+    for passphrase in passphrases:
+        print(passphrase)
